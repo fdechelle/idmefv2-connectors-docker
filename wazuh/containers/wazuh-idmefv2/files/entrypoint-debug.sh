a146a1dd5172ad76450b128b477324a26d6c241b
François Déchelle <fdechelle@teclib.com>
2025-04-24 16:05:09 +0200

    Fix configuration; first tests ok

--- wazuh/containers/wazuh-idmefv2/files/entrypoint-debug.sh
+++ wazuh/containers/wazuh-idmefv2/files/entrypoint-debug.sh
@@ -2,8 +2,8 @@
 
 # install IDMEFv2 connectors
 cd /idmefv2-connectors
-pip install --break-system-packages .
+pip install --break-system-packages --editable .
 
 # debug the Wazuh IDMEFv2 connector
 pip install --break-system-packages debugpy
-python3 -m debugpy -Xfrozen_modules=off --listen 0.0.0.0:5678 --wait-for-client -m idmefv2.connectors.wazuh -c /etc/wazuh-idmefv2.conf
+python3 -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m idmefv2.connectors.wazuh -c /etc/wazuh-idmefv2.conf
