eba430e7221e22d6c84574292a81c98a9d9f9248
François Déchelle <fdechelle@teclib.com>
2025-04-24 14:44:48 +0200

    Add compose for wazuh connector; debug in progress

---
+++ wazuh/containers/wazuh-idmefv2/files/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install IDMEFv2 connectors
+cd /idmefv2-connectors
+pip install --break-system-packages .
+
+# run the Wazuh IDMEFv2 connector
+python3 -m idmefv2.connectors.wazuh -c /etc/wazuh-idmefv2.conf
